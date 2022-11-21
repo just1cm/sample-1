ea7abd98c04955e6e93681a363630209edc77bd1
mqxu <moqi1977@gmail.com>
2022-11-22 07:20:51 +0800

    新增网关服务

---
+++ gateway-service/src/main/java/com/spring/gateway/GatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: mqxu
+ * @date: 2022-11-21
+ **/
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
